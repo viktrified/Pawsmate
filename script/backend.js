a443781ac948892fd2c42c06a33cd85beed64cd7
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-12-07 17:54:17 +0100

    restoring deleted files

--- script/backend.js
+++ script/backend.js
@@ -1,33 +1,34 @@
-class Users {
-  async getUsers() {
-    try {
-      const result = await fetch("../users.json");
-      const data = await result.json();
-      const users = data.dogs.map((dog) => {
-        const { id, breed, age, image, name, sex } = dog;
-        return { id, breed, age, image, name, sex };
-      });
-      return users;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-    }
-  }
-}
+// let submit = document.querySelector("#submitButton");
 
-const usersInstance = new Users();
-usersInstance.getUsers().then((users) => console.log(users));
 
-let submit = document.getElementById("submitButton");
+// class Users {
+//   async getUsers() {
+//     try {
+//       const result = await fetch("../users.json");
+//       const data = await result.json();
+//       const users = data.dogs.map((dog) => {
+//         const { id, breed, age, image, name, sex } = dog;
+//         return { id, breed, age, image, name, sex };
+//       });
+//       return users;
+//     } catch (error) {
+//       console.error("Error fetching users:", error);
+//     }
+//   }
+// }
 
-const genderLogic = () => {
-  let radio = document.getElementsByName("gender");
-  for (i = 0; i < radio.length; i++) {
-    if (radio[0].checked) {
-      console.log("display only bitches");
-    } else {
-      console.log("display niggas");
-    }
-  }
-};
+// const usersInstance = new Users();
+// usersInstance.getUsers().then((users) => console.log(users));
 
-submit.addEventListener("click", genderLogic);
+// const genderLogic = () => {
+//   let radio = document.getElementsByName("gender");
+//   for (i = 0; i < radio.length; i++) {
+//     if (radio[i].checked) {
+//       gender = radio[i];
+//     }
+//   }
+// };
+
+
+// submit.addEventListener("click", genderLogic);
+
