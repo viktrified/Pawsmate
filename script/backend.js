a1a27df7275b2fa284f5ecf2d31f341d0d0ad78d
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-12-07 11:08:46 +0100

    added users image folder

--- script/backend.js
+++ script/backend.js
@@ -1,22 +1,33 @@
-class Products {
-  async getProducts() {
+class Users {
+  async getUsers() {
     try {
-      let result = await fetch("products.json");
-      let data = await result.json();
-      let products = data.item;
-      console.log(typeof item);
-
-      products = products.map((item) => {
-        const { title, price } = item.fields;
-        const { id } = item.sys;
-        const image = item.fields.image.fields.file.url;
-        return { title, price, id, image };
+      const result = await fetch("../users.json");
+      const data = await result.json();
+      const users = data.dogs.map((dog) => {
+        const { id, breed, age, image, name, sex } = dog;
+        return { id, breed, age, image, name, sex };
       });
-      return products;
+      return users;
     } catch (error) {
-      console.log(error);
+      console.error("Error fetching users:", error);
     }
   }
 }
 
+const usersInstance = new Users();
+usersInstance.getUsers().then((users) => console.log(users));
 
+let submit = document.getElementById("submitButton");
+
+const genderLogic = () => {
+  let radio = document.getElementsByName("gender");
+  for (i = 0; i < radio.length; i++) {
+    if (radio[0].checked) {
+      console.log("display only bitches");
+    } else {
+      console.log("display niggas");
+    }
+  }
+};
+
+submit.addEventListener("click", genderLogic);
